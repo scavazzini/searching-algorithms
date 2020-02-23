b02814e80a7137194bc30b3044f0476a8e87f098
Newton Scavazzini <newtonscavazzini@gmail.com>
2020-02-23 03:31:14 -0300

    Linear Search: Implementing the algorithm

---
+++ src/linear-search/linearsearch.cpp
@@ -0,0 +1,12 @@
+int linearsearch(int *arr, int arrSize, int element) {
+
+    for (int i = 0; i < arrSize; i++) {
+
+        if (element == arr[i]) {
+            return i;
+        }
+
+    }
+    return -1;
+
+}
--- src/linear-search/linearsearch.h
+++ src/linear-search/linearsearch.h
@@ -1,1 +1,3 @@
+#include "linearsearch.cpp"
+
 int linearsearch(int *arr, int arrSize, int element);
