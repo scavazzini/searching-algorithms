3c003ec8513a0af4ddf0fd847ae5605132dae968
Newton Scavazzini <newtonscavazzini@gmail.com>
2020-02-23 03:44:33 -0300

    Linear Search: Adding comments on main

--- src/linear-search/main.cpp
+++ src/linear-search/main.cpp
@@ -8,18 +8,23 @@ using namespace std;
 
 int main() {
 
+    // Instantiate array
     int* arr = new int[N];
 
+    // Populating the array
     for (int i = 0; i < N; i++) {
         arr[i] = i;
     }
 
+    // Running the algorithm and measuring time
     auto initialTime = chrono::high_resolution_clock::now();
     int foundAt = linearsearch(arr, N, N);
     auto finalTime = chrono::high_resolution_clock::now();
 
+    // Deallocating array
     delete[] arr;
 
+    // Calculating time and printing results
     auto executionTime =
         chrono::duration_cast<chrono::milliseconds>(finalTime - initialTime); 
 
